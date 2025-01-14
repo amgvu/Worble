3b49f5aa53b99ab3ea9c636486bd0953ce23c474
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-01-13 22:51:02 -0600

    Bot setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log("Token:", process.env.DISCORD_TOKEN);
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
