19e4c6b5537c84af142dd141627297ec8990e99c
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-02-12 23:16:42 -0600

    monkey business

--- src/commands/coffee.ts
+++ src/commands/coffee.ts
@@ -1,9 +1,11 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
-  .setName("coffee")
-  .setDescription("coffee");
+  .setName("monkey")
+  .setDescription("business");
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-  await interaction.reply("https://tenor.com/view/yuimetal-gif-2236335851178412850");
+  await interaction.reply(
+    "https://tenor.com/view/just-monkey-business-gif-13449299463832787070"
+  );
 }
