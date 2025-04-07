57b9fc36e9c15bb010df502aab1d6c42e980e525
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-04-07 04:40:54 -0500

    heroku init

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -7,7 +7,9 @@ dotenv.config();
 
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".ts"));
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts"));
 
 for (const file of commandFiles) {
   const command = require(path.join(commandsPath, file));
@@ -21,11 +23,14 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
     console.log("Started refreshing application (/) commands.");
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!), //remember to change to global commands
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID!,
+        process.env.GUILD_ID!
+      ), //remember to change to global commands
       { body: commands }
-    );    
+    );
 
-    console.log("Successfully reloaded application (/) commands.");
+    console.log("Successfully reloaded application (/) commands!");
   } catch (error) {
     console.error(error);
   }
