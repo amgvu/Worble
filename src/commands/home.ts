4ac82d3684728639c62830224f331a8121de58c4
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-05-27 10:45:02 -0500

    Update home.ts

--- src/commands/home.ts
+++ src/commands/home.ts
@@ -2,7 +2,7 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("dashboard")
-  .setDescription("Provides the link to the app");
+  .setDescription("Provides the link to the dashboard");
 
 export async function execute(interaction: ChatInputCommandInteraction) {
   const dashboardURL = "https://arclify.vercel.app/";
