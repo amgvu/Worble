ae019fa41c8c9655cc9bd2b541535ba0a7fb84ed
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-11 07:32:11 -0500

    deprecate roles router

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import changeNicknameRouter from "./changeNickname";
 import membersRouter from "./members";
 import serversRouter from "./servers";
-import rolesRouter from "./roles";
 
 dotenv.config();
 
@@ -13,6 +12,5 @@ const router = express.Router();
 router.use("/api", changeNicknameRouter);
 router.use("/api", membersRouter);
 router.use("/api", serversRouter);
-router.use("/api", rolesRouter);
 
 export default router;
