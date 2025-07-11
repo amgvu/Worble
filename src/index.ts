b51a564d3c1881cc8056b528edb2ee1088487b30
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-07-11 10:10:36 -0500

    deprecated unused chat commands

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-import { Client, GatewayIntentBits, Collection } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
-import path from "path";
-import fs from "fs";
 import express from "express";
 import routes from "./api/index";
-import { ExtendedClient } from "./types/Client";
 import cors from "cors";
 
 dotenv.config();
 
-export const client: ExtendedClient = new Client({
+export const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
-}) as ExtendedClient;
-
-client.commands = new Collection();
+});
 
 const app = express();
 app.use(express.json());
@@ -31,36 +26,9 @@ app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
 
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".ts"));
-
-for (const file of commandFiles) {
-  const command = require(path.join(commandsPath, file));
-  client.commands.set(command.data.name, command);
-}
-
 client.once("ready", () => {
   console.log(`Logged in as ${client.user?.tag}`);
   client.user?.setStatus("online");
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(`${interaction.commandName} failed to execute:`, error);
-    await interaction.reply({
-      content: "There was an error while executing this command",
-      ephemeral: true,
-    });
-  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- src/types/Client.ts
+++
@@ -1,5 +0,0 @@
-import { Client, Collection } from "discord.js";
-
-export interface ExtendedClient extends Client {
-  commands: Collection<string, any>;
-}
